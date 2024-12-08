f4b5b67e4ee5a3a8eb40c355929d441a757f6136
Alkap <alexkarp0987gmail.com>
2024-12-09 00:06:02 +0300

    gg

--- Astro.py
+++ Astro.py
@@ -1,9 +1,10 @@
+from ast import Constant
+from calendar import LocaleHTMLCalendar
 import pygame
 import math
-import scipy #для констант
+import scipy #РґР»СЏ С„РёР·РёС‡РµСЃРєРёС… РєРѕРЅСЃС‚Р°РЅС‚
 from simpleeval import simple_eval
 
-#единицы измерения км, сек, кг
 
 def vecsum(vect1, vect2):
     return (vect1[0] + vect2[0], vect1[1] + vect2[1])
@@ -14,26 +15,35 @@ def length(vect1):
 def distance(pos1, pos2):
     return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] + pos2[1]) ** 2)
 
+
 a = int(simple_eval(input("The radius of the orbit(a.e.):"))) * 149.6 * 10 ** 6
 i = int(simple_eval(input("The angle to the plane of the orbit:"))) / 57.29577951308
 M1 = int(simple_eval(input("The mass of the first star(Mass of the Sun):"))) * 1.989 * 10 ** 30
 M2 = int(simple_eval(input("The mass of the second star(Mass of the Sun):"))) * 1.989 * 10 ** 30
 
-WIDTH = 800 #1 пиксель = 0.2 ае
+#a = 10 * 149.6 * 10 ** 6
+#i = 0 / 57.29577951308
+#M1 = 5 * 1.989 * 10 ** 30
+#M2 = 5 * 1.989 * 10 ** 30
+
+#РїРµСЂРІР°СЏ Р·РІРµР·РґР° - РєСЂР°СЃРЅР°СЏ
+
+WIDTH = 800 #1 РїРёРєСЃРµР»СЊ = 0.2 Р°Рµ
 HEIGHT = 700
 FPS = 30
 height_graf = 50
-koef = 10 ** 5
-k_len = 5 / 149.6 / 10 ** 6 #коэф для растояний км в пиксели(ае)
+koef = 10 ** 6
+koef1 = int(input())
+k_len = 10 / 149.6 / 10 ** 6 #РѕС‚РЅРѕС€РµРЅРёРµ РїРёРєСЃРµР»РµР№ Рє СЂРµР°Р» РґР»РёРЅРµ
 angle_F = 1
 nak = 0
-grid = 20 #четное
+grid = 20 #пїЅпїЅпїЅпїЅпїЅпїЅ
 
-P = math.sqrt(4 * math.pi * a ** 3 / scipy.constants.G / (M1 + M2)) #из кеплера
-v1 = (0, math.sqrt(scipy.constants.G * M1 / a)) #интеграл энергии
-v2 = (0, math.sqrt(scipy.constants.G * M2 / a)) #интеграл энергии
-pos1 = (-1 * a, 10 / k_len)
-pos2 = (a, -10 / k_len)
+P = math.sqrt(4 * math.pi * a ** 3 / scipy.constants.G / (M1 + M2)) #РїРµСЂРёРѕРґ
+v1 = (0, koef1 * math.sqrt(scipy.constants.G * M1 / a)) #РЅР°С‡Р°Р»СЊРЅР°СЏ СЃРєРѕСЂРѕСЃС‚СЊ
+v2 = (0, -1 * koef1 * math.sqrt(scipy.constants.G * M2 / a)) 
+pos1 = (-1 * a, 0.01)
+pos2 = (a, -0.01)
 F1 = (0, 0)
 F2 = (0, 0)
 
@@ -48,45 +58,48 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 150)
-# Создаем игру и окно
+
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
-# Цикл игры
 nak = math.cos(i)
 running = True
+
+
 while running:
     for event in pygame.event.get():
-        # проверить закрытие окна
             if event.type == pygame.QUIT:
                 running = False
-    #математикa
-    angle_F = math.atan((pos1[0] - pos2[0]) / (pos1[1] - pos2[1]))
-    F1 = (scipy.constants.G / 1000 * M1 * M2 / distance(pos1, pos2) ** 2 * math.cos(angle_F), 
-          scipy.constants.G * M1 * M2 / distance(pos1, pos2) ** 2 * math.sin(angle_F))
 
+
+    #РјР°С‚РµРјР°С‚РёРєР°
+    angle_F = math.atan((pos1[1] - pos2[1]) / (pos1[0] - pos2[0]))
+    F_mg = (scipy.constants.G / 1000) * M1 * M2 / (distance(pos1, pos2) ** 2)
+    F1 = (F_mg * math.cos(angle_F), 
+          F_mg * math.sin(angle_F))
+
+    
     F2 = (F1[0] * -1, F1[1] * -1)
-    v1 = (v1[0] + F1[0] / M1 * koef, v1[1] + F1[1] / M1 * koef)
-    print(angle_F, pos1, pos2)
+    v1 = (v1[0] + F1[0] / M1 * koef, v1[1] + F1[1] / M1 * koef)    
     v2 = (v2[0] + F2[0] / M2 * koef, v2[1] + F2[1] / M2 * koef)
     pos1 = (pos1[0] + v1[0], pos1[1] + v1[1])
     pos2 = (pos2[0] + v2[0], pos2[1] + v2[1])
     
-    #отрисовка
+    #РѕС‚СЂРёСЃРѕРІРєР°
     screen.fill(BLACK)
     
-    for I in range(0, int(grid / 2) + 1):   # сетка
+    for I in range(0, int(grid / 2) + 1):   # СЃРµС‚РєР°
         pygame.draw.line(screen, GRID_CLR, [0, nol[1] + nak * HEIGHT / grid * I], [WIDTH, nol[1] + nak * HEIGHT / grid * I])
         pygame.draw.line(screen, GRID_CLR, [0, nol[1] - nak * HEIGHT / grid * I], [WIDTH, nol[1] - nak * HEIGHT / grid * I])
         pygame.draw.line(screen, GRID_CLR, [WIDTH / grid * I, nol[1] + nol[1] * nak], [WIDTH / grid * I, nol[1] - nol[1] * nak])
         pygame.draw.line(screen, GRID_CLR, [WIDTH / grid * I + nol[0], nol[1] + nol[1] * nak], [WIDTH / grid * I + nol[0], nol[1] - nol[1] * nak])
 
-    pygame.draw.circle(screen, CLR1, [k_len * pos1[0] + nol[0], k_len * nak * pos1[1] + nol[1]], 10)
-    pygame.draw.circle(screen, CLR2, [k_len * pos2[0] + nol[0], k_len * nak * pos2[1] + nol[1]], 10)
+    pygame.draw.circle(screen, CLR1, [k_len * pos1[0] + nol[0], k_len * nak * pos1[1] + nol[1]], 5)
+    pygame.draw.circle(screen, CLR2, [k_len * pos2[0] + nol[0], k_len * nak * pos2[1] + nol[1]], 5)
         
-    #остальное
+    #РѕСЃС‚Р°Р»СЊРЅРѕРµ
     pygame.display.flip()
     clock.tick(FPS)
 

d7458ac72ad6db2df86908c847d0dd74238b7116
Alkap <alexkarp0987gmail.com>
2024-12-20 19:01:04 +0300

    finish0.2

--- Astro.py
+++ Astro.py
@@ -1,16 +1,16 @@
 from ast import Constant
-from calendar import LocaleHTMLCalendar
+from calendar import LocaleHTMLCalendar 
 import pygame
 import math
-import scipy #для физических констант
 from simpleeval import simple_eval
+import sys
 
 
 def vecsum(vect1, vect2):
     return (vect1[0] + vect2[0], vect1[1] + vect2[1])
 
 def length(vect1):
-    return math.sqrt(vect[0] ** 2 + vect[1] ** 2)
+    return math.sqrt(vect1[0] ** 2 + vect1[1] ** 2)
 
 def distance(pos1, pos2):
     return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] + pos2[1]) ** 2)
@@ -22,12 +22,14 @@ def distance(pos1, pos2):
 #M2 = int(simple_eval(input("The mass of the second star(Mass of the Sun):"))) * 1.989 * 10 ** 30
 
 a = 10 * 149.6 * 10 ** 6
-i = 0 / 57.29577951308
+i = 60 / 57.29577951308
 M1 = 5 * 1.989 * 10 ** 30
 M2 = 5 * 1.989 * 10 ** 30
 
 #первая звезда - красная
-
+text1 = "Лучевая скорость, км/с"
+text2 = "Время, с"
+G = 6.6743 * 10 ** -11
 WIDTH = 800 #1 пиксель = 0.2 ае
 HEIGHT = 600
 FPS = 30
@@ -40,16 +42,15 @@ angle_F = 1
 nak = 0
 grid = 20 #четное
 time = 0
+fnt_size = 15
 
-P = math.sqrt(4 * math.pi * a ** 3 / scipy.constants.G / (M1 + M2)) #период
+dt = 30 / FPS
+P = math.sqrt(4 * math.pi * 8 * a ** 3 / G / (M1 + M2)) #период
 V0 = 2 * math.pi * a / P #скалярная величина круг. скорости
-if i = 0:
-    k_graf = 0
-else:
-    max_graf = abs(V0 * math.sin(i)) #максимальное значение графика
-    k_graf = (height_graf - 10) / max_graf
-v1 = (0, koef1 * math.sqrt(scipy.constants.G * M1 / a)) #начальная скорость
-v2 = (0, -1 * koef1 * math.sqrt(scipy.constants.G * M2 / a)) 
+max_graf = abs(V0 * math.sin(i)) #максимальное значение графика
+k_graf = (height_graf - 10) / max_graf
+v1 = (0, koef1 * math.sqrt(G * M1 / a)) #начальная скорость
+v2 = (0, -1 * koef1 * math.sqrt(G * M2 / a)) 
 pos1 = (-1 * a, 0.01)
 pos2 = (a, -0.01)
 F1 = (0, 0)
@@ -57,7 +58,10 @@ F2 = (0, 0)
 
 nol = (WIDTH / 2, HEIGHT / 2)
 cen_graf = (WIDTH / 2, HEIGHT - height_graf)
-T = math.sqrt((4 * math.pi ** 2 * a ** 3) / scipy.constants.G * (M1 + M2))
+pos_text1 = (nol[0], HEIGHT - 2 * height_graf)
+pos_text2 = (WIDTH - 5 * fnt_size, HEIGHT - height_graf)
+
+
 
 GRF_CLR = (197, 208, 230)
 GRID_CLR = (10, 95, 173)
@@ -75,7 +79,12 @@ pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 nak = math.cos(i)
 running = True
+pygame.font.init()
 
+path = pygame.font.match_font("arial")
+Font = pygame.font.Font(path, fnt_size)
+render_text1 = Font.render(text1, 1, (255, 255, 255))
+render_text2 = Font.render(text2, 1, (255, 255, 255))
 
 while running:
     for event in pygame.event.get():
@@ -85,16 +94,16 @@ while running:
 
     #математика
     angle_F = math.atan((pos1[1] - pos2[1]) / (pos1[0] - pos2[0]))
-    F_mg = (scipy.constants.G / 1000) * M1 * M2 / (distance(pos1, pos2) ** 2)
+    F_mg = (G / 1000) * M1 * M2 / (distance(pos1, pos2) ** 2)
     F1 = (F_mg * math.cos(angle_F), 
           F_mg * math.sin(angle_F))
 
     
     F2 = (F1[0] * -1, F1[1] * -1)
-    v1 = (v1[0] + F1[0] / M1 * koef, v1[1] + F1[1] / M1 * koef)    
-    v2 = (v2[0] + F2[0] / M2 * koef, v2[1] + F2[1] / M2 * koef)
-    pos1 = (pos1[0] + v1[0], pos1[1] + v1[1])
-    pos2 = (pos2[0] + v2[0], pos2[1] + v2[1])
+    v1 = (v1[0] + F1[0] / M1 * koef * dt, v1[1] + F1[1] / M1 * koef * dt)    
+    v2 = (v2[0] + F2[0] / M2 * koef * dt, v2[1] + F2[1] / M2 * koef * dt)
+    pos1 = (pos1[0] + v1[0] * dt, pos1[1] + v1[1] * dt)
+    pos2 = (pos2[0] + v2[0] * dt, pos2[1] + v2[1] * dt)
     
     #отрисовка
     screen.fill(BLACK)
@@ -110,16 +119,20 @@ while running:
     pygame.draw.circle(screen, CLR2, [k_len * pos2[0] + nol[0], k_len * nak * pos2[1] + nol[1]], 7)
     
     #график
-    time += 1 / FPS #время в секундах
+    time += dt #время в секундах
 
     pygame.draw.rect(screen, BLACK, (0, HEIGHT - 2 * height_graf, WIDTH, 2 * height_graf))
     pygame.draw.rect(screen, GRF_CLR, (0, HEIGHT - 2 * height_graf, WIDTH, 2 * height_graf), 5)
+    pygame.draw.line(screen, GRF_CLR, (nol[0], HEIGHT - 2 * height_graf), (nol[0], HEIGHT))
+    pygame.draw.line(screen, GRF_CLR, (0, HEIGHT - height_graf), (WIDTH, HEIGHT - height_graf))
 
-    for i_graf in range(0, int(nol[0]) + 1):
+    for i_graf in range(0, int(WIDTH)):
         y1 = k_graf * V0 * math.cos(2 * math.pi * (time + k_grafx * i_graf / FPS) / P) * math.sin(i)
         y2 = k_graf * V0 * math.cos(2 * math.pi * (time + k_grafx * (i_graf - 1) / FPS) / P) * math.sin(i)
-        pygame.draw.line(screen, CLR1, (nol[0] - i_graf, HEIGHT - height_graf - y1), (nol[0] - i_graf - 1, HEIGHT - height_graf - y2))
+        pygame.draw.aaline(screen, CLR1, (WIDTH - i_graf, HEIGHT - height_graf - y1), (WIDTH - i_graf - 1, HEIGHT - height_graf - y2))
+        pygame.draw.aaline(screen, CLR2, (WIDTH - i_graf, HEIGHT - height_graf + y1), (WIDTH - i_graf - 1, HEIGHT - height_graf + y2))
+        screen.blit(render_text1, pos_text1)
+        screen.blit(render_text2, pos_text2)
     #остальное
     pygame.display.flip()
     clock.tick(FPS)
-
